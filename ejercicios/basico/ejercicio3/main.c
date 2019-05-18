08894798ee4cbabac71b1d4e224abc1b444f0c90
Alberto Segura <alberto.segura.delgado@gmail.com>
2019-05-18 19:49:07 +0200

    add basic exercises

---
+++ ejercicios/basico/ejercicio3/main.c
@@ -0,0 +1,13 @@
+#include <stdint.h>
+
+void do_nothing(){
+    char input[120];
+    printf("Do? ");
+    gets(input);
+    printf("Nothing...\n");
+}
+
+int main(int argc, char** argv) {
+    do_nothing();
+    return 0;
+}
